3241dea5edf62bc191c0b39c9c0902d87c1a1d6e
zafarbek-portfol <162569630+bexruzFrontDev@users.noreply.github.com>
2024-05-15 11:59:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let inp = document.querySelectorAll(".colors input");
+let box = document.querySelector(".box");
+let menu = document.querySelector(".select-box select");
+let body = document.body;
+let txt = document.querySelector(".txt");
+let btn = document.querySelector(".copy");
+
+function gradientGenerator() {
+  const gradient = ` linear-gradient(${menu.value}, ${inp[0].value}, ${inp[1].value} )`;
+  box.style.background = gradient;
+  body.style.background = gradient;
+
+  txt.value = `background: ${gradient};`;
+  console.log(gradient);
+}
+
+inp.forEach((el) => {
+  el.addEventListener("input", gradientGenerator);
+});
+
+function kopiya() {
+  navigator.clipboard.writeText(txt.value);
+  btn.innerHTML = "kopiya qilindi!";
+  setTimeout(() => (btn.innerHTML = "kopiya qilish!"), 2000);
+}
+
+menu.addEventListener("change", gradientGenerator);
+btn.addEventListener("click", kopiya);
